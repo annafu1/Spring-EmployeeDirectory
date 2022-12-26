8819560c3ab8f291a2a608233821bbcc0eed48f5
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-12-26 12:08:00 -0500

    added in the other packages

---
+++ src/main/java/io/zipcoder/persistenceapp/controllers/EmployeeController.java
@@ -0,0 +1,14 @@
+package io.zipcoder.persistenceapp.controllers;
+
+import io.zipcoder.persistenceapp.models.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "employee-controller")
+public class EmployeeController {
+
+
+}
--- src/main/java/io/zipcoder/persistenceapp/models/Department.java
+++ src/main/java/io/zipcoder/persistenceapp/models/Department.java
@@ -1,4 +1,4 @@
-package io.zipcoder.persistenceapp;
+package io.zipcoder.persistenceapp.models;
 
 import javax.persistence.*;
 
